0c90c11c4d68e672e933d0087a318011b519f241
Alquama Salim <alquama00s@outlook.com>
2020-12-24 12:07:49 +0530

    added multiple notes fun

--- app/src/main/java/com/codecipher/list/MainActivity.kt
+++ app/src/main/java/com/codecipher/list/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 
 
@@ -13,9 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val add =findViewById<Button>( R.id.button)
         val def =findViewById<TextView>(R.id.textView_default)
+        val scroll=findViewById<LinearLayout>(R.id.scroll)
         val text=findViewById<EditText>(R.id.Text)
         add.setOnClickListener{
-            def.text=text.text;
+            val inflater = this.layoutInflater
+            val dynamic= inflater.inflate(R.layout.a,null,true)
+            dynamic.findViewById<TextView>(R.id.temp).text = text.text
+            scroll.addView(dynamic)
         }
     }
 }
